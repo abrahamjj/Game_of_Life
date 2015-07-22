d8e773cf0e81005ffcab02a9937bf5910b31ea15
abrahamjj <jpa18@pitt.edu>
2015-07-21 23:52:03 -0400

    Changed some imports.

--- src/Controller.java
+++ src/Controller.java
@@ -7,17 +7,16 @@
 
 import java.lang.Math;
 import java.lang.Thread;
-import javax.swing.JSlider;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.awt.event.MouseEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.ActionListener;
-import java.lang.*;
-import java.io.*;
 
 public class Controller {
 
